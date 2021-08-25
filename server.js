065d9d4abb769eb5dc6d222851b988c3723a132b
Glitch (downloader-ml) <none>
2021-08-25 15:57:08 +0000

    🐑🍲 Checkpoint ./index.html:5173917/4243 ./server.js:5173917/136

--- server.js
+++ server.js
@@ -18,10 +18,14 @@ app.get('/download', (req, res) => {
     var url = req.query.url;
     res.header("Content-Disposition", 'attachment;\  filename="Video.mp4');
     ytdl(url, {format: 'best'}).pipe(res);
+    console.log(url)
+  console.log("Downloaded above video")
 })
 
 app.get('/downloadaudio', (req, res) => {
     var url = req.query.url;
     res.header("Content-Disposition", 'attachment;\  filename="Audio.mp3');
     ytdl(url, {format: 'bestaudio'}).pipe(res);
+    console.log(url)
+  console.log("Downloaded above audio")
 })
