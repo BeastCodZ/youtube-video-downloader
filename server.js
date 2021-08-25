bd824f651e1ec95a424ea1f72ef758f27b7ad402
Glitch (downloader-ml) <none>
2021-08-25 09:34:08 +0000

    ☃️🌂 Checkpoint ./index.html:5173917/694 ./script.js:5173917/2286 ./server.js:5173917/477

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
+const ffmpeg = require('fluent-ffmpeg');
+
 app.use('/static', express.static('./static'));
 
 app.listen(process.env.PORT, () => {
@@ -17,9 +19,14 @@ app.get('/download', (req, res) => {
     res.header("Content-Disposition", 'attachment;\  filename="Video.mp4');    
     ytdl(url, {format: 'mp4'}).pipe(res);
 });
-
-app.get('/downloadmp3', (req, res) => {
-    var url = req.query.url;    
+app.get('/convert', (req, res) => {   
+var url = req.query.url;
+let stream = ytdl(url, {quality: 'highestaudio'});
+  ffmpeg(stream).audioBitrate(128)
+  console.log("loading")
+});
+app.get('/downloadmp3', (req, res) => {   
+  var url = req.query.url;
     res.header("Content-Disposition", 'attachment;\  filename="song.mp3');
-    ytdl(url, {filter: 'audioonly'}).pipe(res);
+    ytdl(url, {quality: 'highestaudio'}).pipe(res);
 });
