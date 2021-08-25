a25050910dc8f419eeb48216e7eb7110c1ccf55c
Glitch (glitch-hello-node) <none>
2021-08-25 08:19:49 +0000

    🍼👢 Checkpoint ./server.js:5173917/2687 ./index.html:5173917/3246 ./style.css:5173917/501 ./script.js:5173917/1518

--- server.js
+++ server.js
@@ -2,18 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const ytdl = require('ytdl-core');
 const app = express();
-app.use('/static', express.static('./static'));
-
+app.use(cors());
 app.listen(process.env.PORT, () => {
     console.log("It Works!");
 });
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: './' });
-})
-
-app.get('/download', (req, res) => {
-    var url = req.query.url;
-    res.header("Content-Disposition", 'attachment'; filename='video.mp4');
-    ytdl(url, {format: 'mp4'}).pipe(res);
+app.get('/download', (req,res) => {
+var URL = req.query.URL;
+res.header('Content-Disposition', 'attachment; filename="video.mp4"');
+ytdl(URL, {
+    format: 'mp4'
+    }).pipe(res);
 });
